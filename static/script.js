edb1c2a93dbadeab41e9748bfae9d6b1aba50f9d
ShaneHargrave <shanehargrave123@gmail.com>
2024-12-02 15:47:23 -0700

    Added the structure to solarSystem.html
    Added a possible structure to the html page.

--- static/script.js
+++ static/script.js
@@ -1,3 +1,4 @@
+//Google Code
 (function(){
     var script = document.createElement('script');
     script.src = "https://www.googletagmanager.com/gtag/js?id=G-VRQKTXG4CR";
@@ -12,54 +13,62 @@
     };
 })();
 
-const image1 = 'url("../images/solarSystemBackground.png")';
+const images = [
+    'url("../images/solarSystemBackground.png")',
+    'url("../images/sun.jpg")',
+    'url("../images/mercury.jfif")',
+    'url("../images/venus.png")',
+    'url("../images/earth.jpg")',
+    'url("../images/mars.jpg")',
+    'url("../images/jupiter.jpg")',
+    'url("../images/saturn.jfif")',
+    'url("../images/uranus.jpg")',
+    'url("../images/neptune.png")'
+];
+
+//Star Background
 https://getwallpapers.com/collection/outer-space-stars-wallpaper
 
-const image2 = 'url("../images/sun.jpg")';
+//Sun
 //https://science.nasa.gov/image-detail/amf-gsfc_20171208_archive_e001435/
 
-const image3 = 'url("../images/mercury.jfif")';
+//Mercury
 //https://en.wikipedia.org/wiki/File:Mercury_in_color_-_prockter07_centered.jpg
 
-const image4 = 'url("../images/venus.png")';
+//Venus
 //https://www.nytimes.com/2020/09/19/science/venus-planets-microbes-life.html
 
-const image5 = 'url("../images/earth.jpg")';
-https://davidmiranda.me/earth-rendering/
+//Earth
+//https://davidmiranda.me/earth-rendering/
 
-const image6 = 'url("../images/mars.jpg")';
-https://www.rawpixel.com/image/12696807/big-mars-planet-astronomy-outdoors-generated-image-rawpixel
+//Mars
+//https://www.rawpixel.com/image/12696807/big-mars-planet-astronomy-outdoors-generated-image-rawpixel
 
-const image7 = 'url("../images/jupiter.jpg")';
-https://wallpapers.com/wallpapers/nasa-image-of-jupiter-ur6rikbsqr7uw8q2.html
+//Jupiter
+//https://wallpapers.com/wallpapers/nasa-image-of-jupiter-ur6rikbsqr7uw8q2.html
 
-const image8 = 'url("../images/saturn.jfif")';
+//Saturn
 //https://www.nbcnews.com/slideshow/greatest-hits-cassini-s-incredible-tour-saturn-n749311
 
-const image9 = 'url("../images/uranus.jpg")';
-https://de.wikipedia.org/wiki/Uranus_(Planet)
+//Uranus
+//https://de.wikipedia.org/wiki/Uranus_(Planet)
 
-const image10 = 'url("../images/neptune.png")';
-https://www.wired.com/story/neptune-is-a-windy-chilly-and-baffling-planet-lets-go/
-
-
+//Neptune
+//https://www.wired.com/story/neptune-is-a-windy-chilly-and-baffling-planet-lets-go/
 
 
-window.addEventListener('scroll', function(){
+
+
+scrollThresholds = [1080, 2160, 3240, 4320, 5400, 6480, 7560, 8640, 9720, 10800];
+
+window.addEventListener('scroll', function() {
     let scrollPosition = window.scrollY;
-    let maxScroll = this.document.documentElement.scrollHeight - this.window.innerHeight;
+    let index = scrollThresholds.findIndex(threshold => scrollPosition < threshold);
 
-    let scrollRatio = scrollPosition / maxScroll;
-
-    let backgroundImage;
-
-    if (scrollRatio < 0.35) {
-        backgroundImage = image1;
-    } else if (scrollRatio < 0.65) {
-        backgroundImage = image2;
-    } else {
-        backgroundImage = image3;
+    if (index === -1) {
+        index = scrollThresholds.length - 1;
     }
 
-    this.document.body.style.backgroundImage = backgroundImage;
+    document.body.style.transition = 'background-image 0.5s ease';
+    document.body.style.backgroundImage = images[index];
 });
